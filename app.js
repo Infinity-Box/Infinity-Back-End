7597f5efd8564da4f034fd94143146558e9b2908
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-01-16 20:48:57 -0800

    update css

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express')
+const express = require('express');
 require('dotenv').config();
-const methodOverride = require('method-override')
-const app = express()
+const methodOverride = require('method-override');
+const app = express();
 
 //Mongoose Connection
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Infinity-Back-End', { useNewUrlParser:true});
 
-const Review = require('./models/review')
-const Module = require("./models/module")
+const Review = require('./models/review');
+const Module = require('./models/module');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
@@ -16,8 +16,8 @@ const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 3000;
 app.listen(port);
 
-app.use(express.static(__dirname + '/public'));
-app.use(methodOverride('_method'))
+app.use(express.static(__dirname));
+app.use(methodOverride('_method'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -29,9 +29,9 @@ app.set('view engine', 'handlebars');
 
 require('./controllers/auth.js')(app);
 require('./controllers/modules')(app);
-require('/controllers/reviews')(app);
+require('./controllers/reviews')(app);
 
 module.exports = app;
 module.exports.stop = () => {
     return db.close()
-}
+};
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,26 +1,25 @@
-const express = require('express');
-const app = express();
 const Module = require('../models/module');
 const Review = require('../models/review')
 
-//NEW
-app.post('/modules/reviews', (req, res) => {
-  Review.create(req.body).then(review => {
-    console.log(review);
-    res.redirect(`/modules/${review.moduleId}`);
-  }).catch((err) => {
-    console.log(err.message);
-    });
-});
+module.exports = (app) => {
 
-// DELETE
-app.delete('/modules/reviews/:id', function (req, res) {
-  console.log("DELETE review")
-  Review.findByIdAndRemove(req.params.id).then((review) => {
-    res.redirect(`/modules/${review.moduleId}`);
-  }).catch((err) => {
-    console.log(err.message);
-    });
-});
+  //NEW
+  app.post('/modules/reviews', (req, res) => {
+    Review.create(req.body).then(review => {
+      console.log(review);
+      res.redirect(`/modules/${review.moduleId}`);
+    }).catch((err) => {
+      console.log(err.message);
+      });
+  });
 
-module.exports = app;
+  // DELETE
+  app.delete('/modules/reviews/:id', function (req, res) {
+    console.log("DELETE review")
+    Review.findByIdAndRemove(req.params.id).then((review) => {
+      res.redirect(`/modules/${review.moduleId}`);
+    }).catch((err) => {
+      console.log(err.message);
+      });
+  });
+};
